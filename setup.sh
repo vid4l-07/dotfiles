264b4f585a3e2e7b3d363b7cf99b8499c050a20d
Hugo Vidal <82414351+vid4l-07@users.noreply.github.com>
2025-07-31 20:37:24 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y build-essential ninja-build meson git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev ninja-build nodejs
+sudo apt install -y build-essential ninja-build meson git vim libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev ninja-build nodejs
 
 mkdir content
 
@@ -10,6 +10,8 @@ mkdir ~/.config
 cp -r ./dotfiles-bspwm/bin ~/.config
 
 #bspwm
+echo 'instalando bspwm y sxhkd'
+sleep 1
 cd ./content
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
@@ -28,6 +30,8 @@ cp -r ./dotfiles-bspwm/bspwm ~/.config
 cp -r ./dotfiles-bspwm/sxhkd ~/.config
 
 #polybar
+echo 'instalando polybar'
+sleep 1
 cd ./content
 git clone --recursive https://github.com/polybar/polybar
 cd polybar/
@@ -38,10 +42,13 @@ make -j$(nproc)
 sudo make install
 cd ../..
 
-#picom
+echo 'actualizando e instalando mas paquetes'
+sleep 1
 sudo apt update
-sudo apt install -y libpcre3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
+sudo apt install -y libpcre3-dev libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
 
+#picom
+echo 'instalando picom'
 cd ./content
 git clone https://github.com/ibhagwan/picom.git
 cd picom/
@@ -51,6 +58,8 @@ ninja -C build
 sudo ninja -C build install
 cd ../..
 
+echo 'instalando extras y las nerd fonts'
+sleep 1
 sudo apt install -y rofi feh kitty neovim bat
 
 cp ./dotfiles-bspwm/fonts/* /usr/local/share/fonts/
