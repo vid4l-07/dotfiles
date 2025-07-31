6e38245336bda36b42d1cae6cb2476156a682330
Hugo Vidal <82414351+vid4l-07@users.noreply.github.com>
2025-07-31 18:41:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-#bspwm
 
 sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
 
-cd $HOME/Descargas/
+mkdir ~/.config > /dev/null 2>&
+
+cp -r ./dotfiles-bspwm/bin ~/.config
+
+#bspwm
+cd ~/Descargas
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
 cd bspwm/
@@ -16,11 +20,11 @@ sudo make install
 
 sudo apt install bspwm
 
+cp -r ./dotfiles-bspwm/bspwm ~/.config
+cp -r ./dotfiles-bspwm/sxhkd ~/.config
+
 #polybar
-
-sudo apt install cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
-
-cd $HOME/Descargas
+cd ~/Descargas
 git clone --recursive https://github.com/polybar/polybar
 cd polybar/
 mkdir build
@@ -30,12 +34,10 @@ make -j$(nproc)
 sudo make install
 
 #picom
-
 sudo apt update
-
 sudo apt install libpcre3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
 
-cd $HOME/Descargas
+cd ~/Descargas
 git clone https://github.com/ibhagwan/picom.git
 cd picom/
 git submodule update --init --recursive
@@ -43,55 +45,32 @@ meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
 
-#bspwm
-cp -r dotfiles/bspwm ~/.config/
-chmod +x ~/.config/bspwm/bspwmrc
-
-cp -r dotfiles/sxhkd ~/.config/
-
-#rofi
 sudo apt install rofi
 
-#fonts
-sudo cp dotfiles/fonts* /usr/local/share/fonts/
-sudo unzip /usr/local/share/fonts/*
-sudo fc-cache -v
+cp ./dotfiles-bspwm/fonts/* /usr/local/share/fonts/
+unzip /usr/local/share/fonts/*
+rm /usr/local/share/fonts/*.zip
+cp /usr/local/share/fonts/* /usr/share/fonts/truetype/
+fc-cache -v
 
-
-#wallpaper
 sudo apt install feh
 
-#bin
-cp -r dotfiles/bin ~/.config/
+cp -r ./dotfiles-bspwm/polybar ~/.config
+cp -r ./dotfiles-bspwm/picom ~/.config
+cp -r ./dotfiles-bspwm/rofi ~/.config
 
-#zshrc
-cp dotfiles/zshrc ~/.zshrc
-chmod +x ~/.zshrc
+sudo apt install kitty
+rm -r ~/.config/kitty
+cp -r ./dotfiles-bspwm/kitty ~/.config
+rm ~/.zshrc
+cp ./dotfiles-bspwm/.zshrc ~
+cp ./dotfiles-bspwm/.p10k.zsh ~
+cp -r ./dotfiles-bspwm/powerlevel10k ~/.config
 
-#config polybar
-cp -r dotfiles/polybar ~/.config/
+sudo apt install neovim
+rm -r ~/.vim ~/.vimrc
+cp -r ./dotfiles-bspwm/.vim ~
+cp ./dotfiles-bspwm/.vimrc ~
+cp ./dotfiles-bspwm/nvim ~/.config
 
-#picom
-cp -r dotfiles/picom ~/.config/
-
-#rofi
-cp -r dotfiles/rofi ~/.config/
-
-#p10k
-cp -r dotfiles/powerlevel10k ~/.config
-cp dotfiles/p10k.zsh ~/.p10k.zsh
-chmod +x ~/.p10k.zsh
-
-#nvim
-cp dotfiles/vimrc ~/.vimrc
-chmod +x ~/.vimrc
-
-cp -r dotfiles/vim ~/.vim
-
-sudo apt intall nvim
-cp -r dotfiles/nvim ~/.config/
-cp -r dotfiles/nvim ~/.config/
-
-#kitti
-sudo apt install kitti
-cp -r dotfiles/kitti ~/.config/
+sudo apt install bat
