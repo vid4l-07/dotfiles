8770775214417dad96e6fecfd846e416f1603c50
Hugo Vidal <82414351+vid4l-07@users.noreply.github.com>
2025-07-31 20:10:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt install -y build-essential ninja-build meson git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev ninja-build nodejs
 
-sudo apt install build-essential git vim xcb libxcb-util0-dev libxcb-ewmh-dev libxcb-randr0-dev libxcb-icccm4-dev libxcb-keysyms1-dev libxcb-xinerama0-dev libasound2-dev libxcb-xtest0-dev libxcb-shape0-dev
+mkdir content
 
-mkdir ~/.config > /dev/null 2>&
+mkdir ~/.config
 
 cp -r ./dotfiles-bspwm/bin ~/.config
 
 #bspwm
-cd ~/Descargas
+cd ./content
 git clone https://github.com/baskerville/bspwm.git
 git clone https://github.com/baskerville/sxhkd.git
 cd bspwm/
@@ -18,13 +20,15 @@ cd ../sxhkd/
 make
 sudo make install
 
-sudo apt install bspwm
+sudo apt install -y bspwm
+
+cd ../..
 
 cp -r ./dotfiles-bspwm/bspwm ~/.config
 cp -r ./dotfiles-bspwm/sxhkd ~/.config
 
 #polybar
-cd ~/Descargas
+cd ./content
 git clone --recursive https://github.com/polybar/polybar
 cd polybar/
 mkdir build
@@ -32,20 +36,22 @@ cd build/
 cmake ..
 make -j$(nproc)
 sudo make install
+cd ../..
 
 #picom
 sudo apt update
-sudo apt install libpcre3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
+sudo apt install -y libpcre3-dev meson libxext-dev libxcb1-dev libxcb-damage0-dev libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev libpixman-1-dev libdbus-1-dev libconfig-dev libgl1-mesa-dev libpcre2-dev libevdev-dev uthash-dev libev-dev libx11-xcb-dev libxcb-glx0-dev
 
-cd ~/Descargas
+cd ./content
 git clone https://github.com/ibhagwan/picom.git
 cd picom/
 git submodule update --init --recursive
 meson --buildtype=release . build
 ninja -C build
 sudo ninja -C build install
+cd ../..
 
-sudo apt install rofi
+sudo apt install -y rofi feh kitty neovim bat
 
 cp ./dotfiles-bspwm/fonts/* /usr/local/share/fonts/
 unzip /usr/local/share/fonts/*
@@ -53,13 +59,10 @@ rm /usr/local/share/fonts/*.zip
 cp /usr/local/share/fonts/* /usr/share/fonts/truetype/
 fc-cache -v
 
-sudo apt install feh
-
 cp -r ./dotfiles-bspwm/polybar ~/.config
 cp -r ./dotfiles-bspwm/picom ~/.config
 cp -r ./dotfiles-bspwm/rofi ~/.config
 
-sudo apt install kitty
 rm -r ~/.config/kitty
 cp -r ./dotfiles-bspwm/kitty ~/.config
 rm ~/.zshrc
@@ -67,10 +70,7 @@ cp ./dotfiles-bspwm/.zshrc ~
 cp ./dotfiles-bspwm/.p10k.zsh ~
 cp -r ./dotfiles-bspwm/powerlevel10k ~/.config
 
-sudo apt install neovim
 rm -r ~/.vim ~/.vimrc
 cp -r ./dotfiles-bspwm/.vim ~
 cp ./dotfiles-bspwm/.vimrc ~
-cp ./dotfiles-bspwm/nvim ~/.config
-
-sudo apt install bat
+cp -r ./dotfiles-bspwm/nvim ~/.config
