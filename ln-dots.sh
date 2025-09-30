50e2588ba967f07ed4e70aa546ff5dba187b68df
hvidal <hugovima09@gmail.com>
2025-09-30 22:27:19 +0200

    ln-dots.sh

---
+++ ln-dots.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/bin /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/bspwm /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/kitty /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/nvim /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/.p10k.zsh /home/hvidal/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/picom /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/polybar /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/powerlevel10k /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/rofi /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/sxhkd /home/hvidal/.config/
+ln -s /home/hvidal/datos_lin/entorno/bspwm/config/.zshrc /home/hvidal/
